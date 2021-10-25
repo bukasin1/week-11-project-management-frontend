02161015376a3c6d7e2ea33006a0ea0455cf132b
oluchioraekwe <oluchi.oraekwe@decagon.dev>
2021-10-25 15:40:18 +0100

    second-comit

--- src/Home/HomePage.tsx
+++ src/Home/HomePage.tsx
@@ -1,63 +1,99 @@
+import { useEffect, useState } from "react";
 import "./HomePage.css";
 const HomePage = () => {
-  return (
-    <div className="main">
-      <div className="email-confirmation">
-        CONFIRM YOUR EMAIL ADDRESS BUKASIN1@GMIAL.COM
-      </div>
+  const [userInfo, setUserInfo] = useState<any>({
+    openedTasks: [],
+    closedTasks: [],
+  });
+  //let parsedUser: any;
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    fetch(`https://jaraaa.herokuapp.com/profile`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `${token}`,
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => setUserInfo(data.sendUser));
+  }, []);
+  const user: any = localStorage.getItem("user");
+  const parsedUser = JSON.parse(user);
+  console.log("logged in user home page: ", userInfo.openedTasks);
+  const closedTasks = userInfo.closedTasks;
+  const openTasks = userInfo.openedTasks;
 
+  const todayTasks = openTasks.filter(
+    (task: any) =>
+      task.updatedAt.toString().split("T")[0].substring(0, 15) ===
+      new Date().toString().split("T")[0].substring(0, 15)
+  );
+  const dateToday = new Date().toString().split("T")[0].substring(0, 15);
+  console.log("today date", dateToday);
+  console.log("today task for loggged User", todayTasks);
+  console.log("closed task for loggged User", closedTasks);
+  console.log("open task for loggged User", openTasks);
+  return (
+    <div className="main-homepage">
       <div className="home-main-container">
         <div className="Top">
           <div className="number-of-tasks">
             <h2 style={{ fontSize: "22px", fontWeight: 500 }}>
-              Completed Tasks
+              {userInfo.closedTasks.length > 1
+                ? "Completed Tasks"
+                : "Completed Task"}
             </h2>
-            <p style={{ fontSize: "50px", fontWeight: 700 }}>370</p>
+            <p style={{ fontSize: "50px", fontWeight: 700 }}>
+              {userInfo.closedTasks.length}
+            </p>
           </div>
-          <div className="tsks-logo">Task Logo</div>
+          <div className="tsks-logo">
+            <img
+              className="task-logo-home"
+              src="https://reactscript.com/wp-content/uploads/2017/01/React-wrapper-for-Chart.js-2.png"
+              alt="Chart Logo"
+            />
+          </div>
         </div>
         <div className="Middle">
           <div className="today-task-main">
-            <div className="today-task">Today Tasks</div>
-            <div className="add-button">
-              <button>+Add Task</button>
+            <div className="today-task">
+              {todayTasks.length === 1 ? (
+                "Today Task"
+              ) : todayTasks.length > 1 ? (
+                "Today Tasks"
+              ) : (
+                <h2 className="no-task-today">No Task Assigned for today</h2>
+              )}
             </div>
           </div>
           <div className="middle-content-container">
-            <div className="middle-content">
-              Email after registration so that I can confirm it
-            </div>
-            <div className="middle-content">
-              Email after registration so that I can confirm it
-            </div>
-            <div className="middle-content">
-              Email after registration so that I can confirm it
-            </div>
-            <div className="middle-content">
-              Email after registration so that I can confirm it
-            </div>
-            <div className="middle-content">
-              Email after registration so that I can confirm it
-            </div>
+            {todayTasks.map((task: any) => (
+              <div className="middle-content" key={task._id}>
+                {task.title}
+              </div>
+            ))}
           </div>
         </div>
         <div className="Bottom">
           <div className="pending-task-main">
-            <div className="pending-task">Pending Tasks</div>
+            <div className="pending-task">
+              {openTasks.length === 1 ? (
+                "Pending Task"
+              ) : openTasks.length > 1 ? (
+                "Pending Tasks"
+              ) : (
+                <h2 className="no-task-today">No Pending Task</h2>
+              )}
+            </div>
           </div>
           <div className="bottom-content-container">
-            <div className="bottom-content">
-              Find Top 5 customers and get reviews from them
-            </div>
-            <div className="bottom-content">
-              Find Top 5 customers and get reviews from them
-            </div>
-            <div className="bottom-content">
-              Find Top 5 customers and get reviews from them
-            </div>
-            <div className="bottom-content">
-              Find Top 5 customers and get reviews from them
-            </div>
+            {openTasks.map((task: any) => (
+              <div className="bottom-content" key={task._id}>
+                {task.title}
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- src/Sidebar/Home.tsx
+++ src/Sidebar/Home.tsx
@@ -1,19 +1,18 @@
+import HomePage from "../Home/HomePage";
 import { Navbar } from "./navbar";
 import Side from "./Side";
+import "./Home.css";
 
 export default function HomeComponent(props: any) {
-    return (
-        <>
-            <Side/>
-            <div className="content">
-
-                <Navbar />
-                <div className="test">
-                    
-
-                </div>
-
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Side />
+      <div className="content">
+        <Navbar />
+        <div className="test">
+          <HomePage />
+        </div>
+      </div>
+    </>
+  );
 }
