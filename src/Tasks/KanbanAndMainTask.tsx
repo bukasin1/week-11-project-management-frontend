ad85155c668e9a656f010d58db6f79ec03299cb5
Decagon <maximmajor4us@gmail.com>
2021-10-26 14:16:07 +0100

    updatedMergedWork

--- src/Tasks/KanbanAndMainTask.tsx
+++ src/Tasks/KanbanAndMainTask.tsx
@@ -1,11 +1,15 @@
 import Kanban from "./Kanban";
 import Maintask from "./Maintask";
+import  "./Maintask.css"
 
 export default function KanbanAndMainTaks() {
   return (
-    <>
+    <div className="TaskContents">
+      
       <Kanban />
+      <div className= "taskSeperator"></div>
       <Maintask />
-    </>
+    
+    </div>
   );
 }
